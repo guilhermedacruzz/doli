6a44ad5f81d189847b34ae7157f4e6f60b4d1e4b
indexGuilherme <guidacruz2004@gmail.com>
2022-02-15 20:41:10 -0300

    Fazendo o botão copiar e colar funcionar

---
+++ js/copy_paste.js
@@ -0,0 +1,8 @@
+document.getElementById("input-bitcoin").value = "14DVQErWme4PBWQJyY7CR2SEQhivapRz4p"
+document.getElementById("input-ethereum").value = "0x24b42a45b8cc67061c251fdfed771a05a36605e5"
+document.getElementById("input-pix").value = "358246c5-4865-4286-906a-708e098eaaca"
+
+function copyAndPaste(value) {
+    navigator.clipboard.writeText(value)
+}
+
